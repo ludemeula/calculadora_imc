1db42de221d4a6cfa04c10808c390e2f6fa712a7
ludemeula <ludemeula@gmail.com>
2019-06-26 22:45:27 -0300

    Updating layout

--- lib/main.dart
+++ lib/main.dart
@@ -15,53 +15,56 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:
-            Text('Calculadora de IMC', style: TextStyle(color: Colors.black)),
-        centerTitle: true,
-        backgroundColor: Color(0xff81b9bf),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.refresh, color: Colors.black),
-            onPressed: () {},
-          ),
-        ],
-      ),
-
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Row(
+        appBar: AppBar(
+          title:
+              Text('Calculadora de IMC', style: TextStyle(color: Colors.black)),
+          centerTitle: true,
+          backgroundColor: Color(0xff81b9bf),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh, color: Colors.black),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Icon(Icons.person_outline, size: 70),
-              new Flexible(
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      labelText: 'Peso (kg)',
-                      helperText: 'Insira o Peso (kg)',
-                      labelStyle: TextStyle(color: Colors.black)),
-                  style: TextStyle(fontSize: 20, color: Colors.black),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: 'Peso (kg)',
+                    helperText: 'Insira o Peso (kg)',
+                    labelStyle: TextStyle(color: Colors.black)),
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: 'Altura (cm)',
+                    helperText: 'Insira a Altura (cm)',
+                    labelStyle: TextStyle(color: Colors.black)),
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+              Container(
+                height: 50,
+                margin: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Calcular'),
+                  color: Color(0xffb2ebf2),
                 ),
               ),
-              new Flexible(
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      labelText: 'Altura (cm)',
-                      helperText: 'Insira a Altura (cm)',
-                      labelStyle: TextStyle(color: Colors.black)),
-                  style: TextStyle(fontSize: 20, color: Colors.black),
-                ),
-              ),
+              Text(
+                'Info',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Color(0xffb2ebf2), fontSize: 25),
+              )
             ],
           ),
-          RaisedButton(
-            onPressed: () {},
-            child: Text('Calcular'),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
