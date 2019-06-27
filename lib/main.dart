ad390989d96e13d828ac006373ddebf875d8e882
ludemeula <ludemeula@gmail.com>
2019-06-26 21:59:02 -0300

    Adding calculate button

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,49 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Calculadora de IMC'),
+        title:
+            Text('Calculadora de IMC', style: TextStyle(color: Colors.black)),
         centerTitle: true,
-        backgroundColor: Colors.deepOrangeAccent,
+        backgroundColor: Color(0xff81b9bf),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.refresh),
+            icon: Icon(Icons.refresh, color: Colors.black),
             onPressed: () {},
+          ),
+        ],
+      ),
+
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Icon(Icons.person_outline, size: 70),
+              new Flexible(
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: 'Peso (kg)',
+                      helperText: 'Insira o Peso (kg)',
+                      labelStyle: TextStyle(color: Colors.black)),
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+              ),
+              new Flexible(
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: 'Altura (cm)',
+                      helperText: 'Insira a Altura (cm)',
+                      labelStyle: TextStyle(color: Colors.black)),
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+              ),
+            ],
+          ),
+          RaisedButton(
+            onPressed: () {},
+            child: Text('Calcular'),
           ),
         ],
       ),
